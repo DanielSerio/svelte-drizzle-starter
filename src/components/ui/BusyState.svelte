a57bdcf3d517799e4a715d7b1849183f6a07708e
DanielSerio <dserio.d@gmail.com>
2024-08-20 16:42:08 -0400

    busy state component

---
+++ src/components/ui/BusyState.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	export let message: string = 'Loading...';
+</script>
+
+<div class="busy-state">
+	<div class="loader">
+		<svg viewBox="0 0 14 14">
+			<circle cx={7} cy={7} r={6} stroke-width="0.5" fill="transparent" stroke="var(--border-28)">
+				<animate
+					attributeName="opacity"
+					values="0;1"
+					dur="1s"
+					repeatCount="indefinite"
+					begin="0.125s;op.end + 0.125s;"
+				/>
+				<animate
+					attributeName="r"
+					values="0;7"
+					dur="1s"
+					repeatCount="indefinite"
+					begin="0.125s;op.end + 0.125s;"
+				/>
+				<animate
+					attributeName="opacity"
+					values="1;0"
+					dur="1s"
+					repeatCount="indefinite"
+					begin="0.125s;op.end + 0.125s;"
+				/>
+			</circle>
+			<circle cx={7} cy={7} r={4} stroke-width="0.75" fill="transparent" stroke="var(--border-28)">
+				<animate attributeName="opacity" values="0;1" dur="1s" repeatCount="indefinite" />
+				<animate attributeName="r" values="0;7" dur="1s" repeatCount="indefinite" />
+				<animate attributeName="opacity" values="1;0" dur="1s" repeatCount="indefinite" />
+			</circle>
+			<circle cx={7} cy={7} r={2} fill="var(--border-28)">
+				<animate
+					attributeName="opacity"
+					values="0;0.8;0"
+					dur="1s"
+					begin="0.25s;op.end + 0.25s;"
+					repeatCount="indefinite"
+				/>
+			</circle>
+		</svg>
+	</div>
+	<small>{message}</small>
+</div>
---
+++ src/routes/(public)/confirm/[slug]/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import LayoutPage from '$components/layout/LayoutPage.svelte';
+	import PageSection from '$components/layout/PageSection.svelte';
+	import BusyState from '$components/ui/BusyState.svelte';
+</script>
+
+<LayoutPage name="confirm-account">
+	<PageSection id="processing">
+		<BusyState message="Processing" />
+	</PageSection>
+</LayoutPage>
